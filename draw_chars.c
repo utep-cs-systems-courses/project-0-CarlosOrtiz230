95d90d6bb8055505aaf675cbd1e62597d3ff025c
Carlos Daniel Ortiz <cdortiz2@miners.utep.edu>
2023-06-21 12:07:24 -0600

    update project 0

--- draw_chars.c
+++ draw_chars.c
@@ -1,6 +1,7 @@
 #include "draw.h"		/* for font */
 #include "stdio.h"		/* for putchar */
 
+
 void print_char_11x16(char c)
 {
   c -= 0x20;
@@ -13,3 +14,15 @@ void print_char_11x16(char c)
     putchar('\n');
   }
 }
+
+void  print_newFont_11x16(char c){
+   c -=0x20 ;
+     for (char row = 0; row < 11; row++) {
+    unsigned short rowBits = newFont_11x16[c][row];
+    for (char col = 0; col < 16; col++) {
+      unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+ }   //print new fotn ends
